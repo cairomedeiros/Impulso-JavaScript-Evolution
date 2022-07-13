c975c96387345e540c802239e4fd3cde929aa8b6
cairomedeiros <cairomedeiros13@gmail.com>
2022-07-13 15:37:18 -0300

    search bar

--- desafio-de-código-reactjs/src/components/SearchBar/index.jsx
+++ desafio-de-código-reactjs/src/components/SearchBar/index.jsx
@@ -1,11 +1,32 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchAsyncUser, fetchAsyncRepos, fetchAsyncReposStarred } from '../../features/repos/repoSlice';
+
 import './style.css';
 
 function SearchBar() {
+  const dispatch = useDispatch("");
+
+  const [term, setTerm] = useState("");
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(fetchAsyncUser(term));
+    dispatch(fetchAsyncRepos(term));
+    dispatch(fetchAsyncReposStarred(term));
+  }
   
     return (
-      <form className="search">
-        <input className='inputStyle' type="text" placeholder="Digite um nome de usuário..."/>
+      <form className="search" onSubmit={submitHandler}>
+        <input 
+        className='inputStyle' 
+        type="text"
+        value={term}
+        placeholder="Digite um nome de usuário..."
+        onChange={(e) => 
+          setTerm(e.target.value)
+        }/>
         <button type='submit'>Buscar</button>
+        
       </form>
        
     )
--- desafio-de-código-reactjs/src/features/repos/repoSlice.jsx
+++ desafio-de-código-reactjs/src/features/repos/repoSlice.jsx
@@ -1,21 +1,21 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import api from '../../api/api';
 
-export const fetchAsyncRepos = createAsyncThunk('repos/fetchAsyncRepos', async () => {
-    const users = "cairomedeiros";
-    const response = await api.get(`/users/${users}/repos?per_page=20`);
+export const fetchAsyncRepos = createAsyncThunk('repos/fetchAsyncRepos', async (term) => {
+    
+    const response = await api.get(`/users/${term}/repos?per_page=20`);
       
     return response.data;
 });
-export const fetchAsyncReposStarred = createAsyncThunk('repos/fetchAsyncReposStarred', async () => {
+export const fetchAsyncReposStarred = createAsyncThunk('repos/fetchAsyncReposStarred', async (term) => {
     const users = "cairomedeiros";
-    const response = await api.get(`/users/${users}/starred?per_page=20`);
+    const response = await api.get(`/users/${term}/starred?per_page=20`);
       
     return response.data;
 });
-export const fetchAsyncUser = createAsyncThunk('repos/fetchAsyncUser', async () => {
+export const fetchAsyncUser = createAsyncThunk('repos/fetchAsyncUser', async (term) => {
     const users = "cairomedeiros";
-    const response = await api.get(`/users/${users}`);
+    const response = await api.get(`/users/${term}`);
       
     return response.data;
 });
